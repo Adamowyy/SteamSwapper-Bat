d3491a1c931d7d1feb54612a4ec01af53c48c2a8
Adamowyy <68386976+Adamowyy@users.noreply.github.com>
2024-01-22 18:03:36 +0100

    One account SteamSwapper
    minor corrections

--- YourUsernameSwapper.bat
+++ YourUsernameSwapper.bat
@@ -8,30 +8,36 @@ for /f "tokens=3" %%i in ('reg query "HKCU\Software\Valve\Steam" /v AutoLoginUse
 		echo -----
 		pause
 		exit
-rem --- ..and if the current user is not the same as filename, we can change the account ---
     ) else (
 		powershell -window minimized -command ""
-rem --- Kill the Steam process and set the username from filename in AutoLogin so that Steam recognises your saved account. ---
-		taskkill.exe /F /IM steam.exe
+		rem --- Check if the Steam process is running and kill it ---
+    	tasklist /FI "IMAGENAME eq steam.exe" 2>NUL | find /I "steam.exe">NUL
+    	if "%ERRORLEVEL%"=="0" (
+        	taskkill.exe /F /IM steam.exe
+    	)
+		rem --- Set the username in AutoLogin so that Steam recognises your saved account and open steam ---
         reg add "HKCU\Software\Valve\Steam" /v AutoLoginUser /t REG_SZ /d "%~n0" /f
         reg add "HKCU\Software\Valve\Steam" /v RememberPassword /t REG_DWORD /d 1 /f
+		cls
+		echo Shh... Don't look. I will start Steam for you. Just wait..
 		timeout /t 3 /nobreak  >nul 2>&1
         start steam://open/main
-rem (If you wish, you can delete from here..)
-rem --- This code will minimise the Steam window to the system tray when opened (If you wish, you can delete this)
+		
+		rem 1. (If you wish, you can delete from here..)
+		rem --- This code will minimise the Steam window to the system tray when opened (If you wish, you can delete this)
 		timeout /t 10 /nobreak  >nul 2>&1
         tasklist /FI "IMAGENAME eq steamwebhelper" 2>NUL | find /I /N "steamwebhelper">NUL
         if "%ERRORLEVEL%"=="0" (
             powershell -command "(Get-Process -Name 'steamwebhelper').MainWindowHandle | ForEach-Object { Add-Type -MemberDefinition '[DllImport(\"user32.dll\")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);' -Name Win32 -Namespace Utils -PassThru | Out-Null; [Utils.Win32]::ShowWindowAsync($_, 0) | Out-Null }"
         )
-rem (..to there)
-
-rem --- If you want to start your game after switching account, simply remove "rem" below and add the correct game location.exe. ---
+		rem 1. (..to there)
+		
+		rem --- Optional: Start a game after switching account (REMOVE "REM" from "START" below) ---
         rem start "Game" "C:\Program Files (x86)\Steam\steamapps\common\Game.exe"
+		
 		exit
     )
-	powershell -window normal -command ""
-	echo Script launch failed!
+	echo Script launch failed. Try first logging in to Steam.
 	echo If you need help: (Discord: xadamowy or Telegram: @AdamowY)
 	pause
 	exit
