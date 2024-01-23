f1fde94833ecef1ec913856f0e0f026e7d05f953
Adamowyy <68386976+Adamowyy@users.noreply.github.com>
2024-01-23 19:39:15 +0100

    One Account SteamSwapper
    Minor improvements

--- YourUsernameSwapper.bat
+++ YourUsernameSwapper.bat
@@ -1,18 +1,40 @@
 @echo off
+setlocal enabledelayedexpansion
 
 rem --- First we will check your logged in account.. ---
 for /f "tokens=3" %%i in ('reg query "HKCU\Software\Valve\Steam" /v AutoLoginUser 2^>nul ^| find "REG_SZ"') do (
     if "%%i"=="%~n0" (
-		echo -----
-		echo You are already logged in as "%~n0"!
-		echo -----
-		pause
-		exit
+		tasklist /FI "IMAGENAME eq steam.exe" 2>NUL | find /I /N "steam.exe">NUL
+		if "!ERRORLEVEL!"=="0" (
+			color 0C
+			echo -----
+        	echo You are already logged in as "%~n0".
+        	echo -----
+			pause
+			exit
+		) else (
+			color 0E
+			echo -----
+        	echo You are already logged in as "%~n0", but Steam is not running.
+        	echo -----
+			echo Open Steam?
+			echo.
+			echo 1. Yes
+			echo 2. No
+			echo.
+            CHOICE /C 12 /N /M "Enter your choice:"
+			if errorlevel 2 (
+    			exit
+			) else (
+    			start steam://open/main
+    			exit
+			)
+		)
     ) else (
 		powershell -window minimized -command ""
 		rem --- Check if the Steam process is running and kill it ---
-    	tasklist /FI "IMAGENAME eq steam.exe" 2>NUL | find /I "steam.exe">NUL
-    	if "%ERRORLEVEL%"=="0" (
+		tasklist /FI "IMAGENAME eq steam.exe" 2>NUL | find /I /N "steam.exe">NUL
+    	if "!ERRORLEVEL!"=="0" (
         	taskkill.exe /F /IM steam.exe
     	)
 		rem --- Set the username in AutoLogin so that Steam recognises your saved account and open steam ---
@@ -26,10 +48,10 @@ for /f "tokens=3" %%i in ('reg query "HKCU\Software\Valve\Steam" /v AutoLoginUse
 		rem 1. (If you wish, you can delete from here..)
 		rem --- This code will minimise the Steam window to the system tray when opened (If you wish, you can delete this)
 		timeout /t 10 /nobreak  >nul 2>&1
-        tasklist /FI "IMAGENAME eq steamwebhelper" 2>NUL | find /I /N "steamwebhelper">NUL
-        if "%ERRORLEVEL%"=="0" (
-            powershell -command "(Get-Process -Name 'steamwebhelper').MainWindowHandle | ForEach-Object { Add-Type -MemberDefinition '[DllImport(\"user32.dll\")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);' -Name Win32 -Namespace Utils -PassThru | Out-Null; [Utils.Win32]::ShowWindowAsync($_, 0) | Out-Null }"
-        )
+		tasklist /FI "WINDOWTITLE eq Steam" 2>NUL | find /I "Steam" >NUL
+		if "!ERRORLEVEL!"=="0" (
+    		taskkill /FI "WINDOWTITLE eq Steam"
+		)
 		rem 1. (..to there)
 		
 		rem --- Optional: Start a game after switching account (REMOVE "REM" from "START" below) ---
@@ -37,8 +59,8 @@ for /f "tokens=3" %%i in ('reg query "HKCU\Software\Valve\Steam" /v AutoLoginUse
 		
 		exit
     )
-	echo Script launch failed. Try first logging in to Steam.
-	echo If you need help: (Discord: xadamowy or Telegram: @AdamowY)
+	echo Script launch failed.
+	echo If you need help: Discord: xadamowy or Telegram: @AdamowY
 	pause
 	exit
 )
