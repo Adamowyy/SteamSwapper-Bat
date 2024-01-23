45f0593b270899bc27bc9d320e2afc1c138d71c1
Adamowyy <68386976+Adamowyy@users.noreply.github.com>
2024-01-23 19:37:39 +0100

    Multiple Account SteamSwapper
    Minor improvements

--- SteamSwapper.bat
+++ SteamSwapper.bat
@@ -5,6 +5,7 @@ TITLE SteamSwapper
 
 rem --- First we will check your logged in account.. ---
 for /f "tokens=3" %%i in ('reg query "HKCU\Software\Valve\Steam" /v AutoLoginUser 2^>nul ^| find "REG_SZ"') do (
+	color 0E
     echo -----
     echo You are already logged in as "%%i"!
     echo -----
@@ -17,26 +18,29 @@ for /f "tokens=3" %%i in ('reg query "HKCU\Software\Valve\Steam" /v AutoLoginUse
 	rem echo 4. "PutYour4thUsername"
 
     echo -----
-    set /P "Select=Enter the number:"
+    set /P "Select=Enter the number: "
 
  	rem -!- PUT YOUR STEAM USERNAME BELOW -!- 
     if "!Select!"=="1" ( set "login=YourUsername1" )
-    if "!Select!"=="2" ( set "login=YourUsername1" )
+    if "!Select!"=="2" ( set "login=YourUsername2" )
 	rem if "!Select!"=="3" ( set "login=PutYour3rdUsername ")
 	rem if "!Select!"=="4" ( set "login=PutYour4thUsername ")
 
 	if "!login!"=="%%i" (
 		cls
+		color 0C
 		echo -----
-		echo You are already logged in to this account. Choose another one..
+		echo You are already logged in to this account. Choose another one.
 		echo -----
 		pause
 		exit
 	) else (
 		if "!login!"=="" (
 			cls
+			color 0C
 			echo -----
-			echo You don't have "!Select!" account. Choose another one..
+			echo You don't have "!Select!" account.
+			echo Having trouble adding another account? I'll help - Discord: xadamowy or Telegram: @AdamowY
 			echo -----
 			pause
 			exit
@@ -44,7 +48,7 @@ for /f "tokens=3" %%i in ('reg query "HKCU\Software\Valve\Steam" /v AutoLoginUse
 			powershell -window minimized -command ""
 			rem --- Check if the Steam process is running and kill it ---
     		tasklist /FI "IMAGENAME eq steam.exe" 2>NUL | find /I "steam.exe">NUL
-    		if "%ERRORLEVEL%"=="0" (
+    		if "!ERRORLEVEL!"=="0" (
         		taskkill.exe /F /IM steam.exe
     		)
 
@@ -59,17 +63,17 @@ for /f "tokens=3" %%i in ('reg query "HKCU\Software\Valve\Steam" /v AutoLoginUse
 			rem (If you wish, you can delete from here..)
 			rem --- This code will minimise the Steam window to the system tray when opened (If you wish, you can delete this)
 			timeout /t 10 /nobreak  >nul 2>&1
-			tasklist /FI "IMAGENAME eq steamwebhelper" 2>NUL | find /I /N "steamwebhelper">NUL
-			if "%ERRORLEVEL%"=="0" (
-				powershell -command "(Get-Process -Name 'steamwebhelper').MainWindowHandle | ForEach-Object { Add-Type -MemberDefinition '[DllImport(\"user32.dll\")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);' -Name Win32 -Namespace Utils -PassThru | Out-Null; [Utils.Win32]::ShowWindowAsync($_, 0) | Out-Null }"
+			tasklist /FI "WINDOWTITLE eq Steam" 2>NUL | find /I "Steam" >NUL
+			if "!ERRORLEVEL!"=="0" (
+    			taskkill /FI "WINDOWTITLE eq Steam"
 			)
-			rem (..to there)
+			rem 1. (..to there)
 
 		exit
 		)
 	)
-	echo Script launch failed. Try first logging in to Steam.
-	echo If you need help: (Discord: xadamowy or Telegram: @AdamowY)
+	echo Script launch failed.
+	echo If you need help: Discord: xadamowy or Telegram: @AdamowY
 	pause
 	exit
 )
